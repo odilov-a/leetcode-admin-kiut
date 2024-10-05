95efd05da424a8480c0860b9578031052db2c7bb
Akbar Odilov <akbarjonodilov01@gmail.com>
2024-10-05 15:15:42 +0500

    Refactor navigation component to update logo size

--- src/components/navigation2/index.tsx
+++ src/components/navigation2/index.tsx
@@ -43,7 +43,7 @@ const Navigation2: React.FC = () => {
     <div className="h-full bg-no-repeat bg-cover bg-left">
       <div className="flex justify-center text-center cursor-pointer pt-[30px] pb-[30px] text-[#9EA3B5]">
         <Link to="/">
-          <img className="w-[50px]" src={logo} alt="logo" />
+          <img className="w-[40px]" src={logo} alt="logo" />
         </Link>
       </div>
       <div className={"h-[80vh] overflow-y-scroll no-scrollbar"}>
