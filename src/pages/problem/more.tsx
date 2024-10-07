8c099f27997f8d877ea0f0fd55277bcdc7e7f7a3
Akbar Odilov <akbarjonodilov01@gmail.com>
2024-10-07 16:57:36 +0500

    Refactor test case output in problem more page

--- src/pages/problem/more.tsx
+++ src/pages/problem/more.tsx
@@ -65,7 +65,7 @@ const More = ({ showMoreModal, moreModal }: any) => {
                 <ul className="list-disc pl-4">
                   {data.testCases.map((testCase: any) => (
                     <li key={testCase._id}>
-                      <p><b>Input:</b> {testCase.input} | <b>Output:</b> {testCase.expectedOutput}</p>
+                      <p><b>Input:</b> {testCase.input} | <b>Output:</b> {testCase.output}</p>
                     </li>
                   ))}
                 </ul>
@@ -81,99 +81,3 @@ const More = ({ showMoreModal, moreModal }: any) => {
 };
 
 export default More;
-
-
-// import { useHooks } from "hooks";
-
-// const More = ({ showMoreModal, moreModal }: any) => {
-//   const data = moreModal?.data; // Accessing the first element of the data array
-//   const { t } = useHooks();
-
-//   return (
-//     <div>
-//       <div className="flex">
-//         <div className="mr-[30px]">
-//           <div className="flex items-center mb-[10px]">
-//             <p className="mr-[20px]">
-//               <p>{t("title")}:</p>
-//             </p>
-//             <b>{data.title}</b>
-//           </div>
-//           <div className="flex items-center mb-[10px]">
-//             <p className="mr-[20px]">
-//               <p>{t("description")}:</p>
-//             </p>
-//             <b>{data.description}</b>
-//           </div>
-//           <div className="flex items-center mb-[10px]">
-//             <p className="mr-[20px]">
-//               <p>{t("difficulty")}:</p>
-//             </p>
-//             <b>{data.difficulty.title}</b>
-//           </div>
-//           <div className="flex items-center mb-[10px]">
-//             <p className="mr-[20px]">
-//               <p>{t("subject")}:</p>
-//             </p>
-//             <b>{data.subject.title}</b>
-//           </div>
-//           <div className="flex items-center mb-[10px]">
-//             <p className="mr-[20px]">
-//               <p>{t("timeLimit")}:</p>
-//             </p>
-//             <b>{data.timeLimit} {t("ms")}</b>
-//           </div>
-//           <div className="flex items-center mb-[10px]">
-//             <p className="mr-[20px]">
-//               <p>{t("memoryLimit")}:</p>
-//             </p>
-//             <b>{data.memoryLimit} {t("MB")}</b>
-//           </div>
-//           <div className="flex items-center mb-[10px]">
-//             <p className="mr-[20px]">
-//               <p>{t("point")}:</p>
-//             </p>
-//             <b>{data.point} {t("ball")}</b>
-//           </div>
-//           <div className="flex items-center mb-[10px]">
-//             <p className="mr-[20px]">
-//               <p>{t("tutorials")}:</p>
-//             </p>
-//             <div>
-//               {data.tutorials && data.tutorials.length > 0 ? (
-//                 <ul>
-//                   {data.tutorials.map((sub: any, index: number) => (
-//                     <b><li key={index}>{sub}</li></b>
-//                   ))}
-//                 </ul>
-//               ) : (
-//                 <b>{t("No tutorials available")}</b>
-//               )}
-//             </div>
-//           </div>
-//           <div className="flex items-center mb-[10px]">
-//             <p className="mr-[20px]">
-//               <p>{t("testCases")}:</p>
-//             </p>
-//             <div>
-//               {data.testCases && data.testCases.length > 0 ? (
-//                 <ul className="flex mr-[10px]">
-//                   {data.testCases.map((testCase: any) => (
-//                     <li key={testCase._id}>
-//                       <p>Input:</p> <b>{testCase.input}</b> | 
-//                       <p>Output:</p> <b>{testCase.expectedOutput}</b>
-//                     </li>
-//                   ))}
-//                 </ul>
-//               ) : (
-//                 <b>{t("No test cases available")}</b>
-//               )}
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default More;
